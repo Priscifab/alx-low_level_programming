5dc6e59ca1590d87c0aab38861f6d53cda6fb567
Prisicfab <106880543+Priscifab@users.noreply.github.com>
2022-10-15 13:59:49 -0700

    hm

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-	Printf("Sizeof a char: %id byte(s)\n", sizeof(char));
-		Printf("Sizeof an int: %id byte(s)\n", sizeof(int));
-			Print("Sizeof a long int: %id bytes(s)\n", sizeof(long int));
-				Printf("Sizeof long long int: %id byte(s)\n", sizeof(long long int));
-					Printf("Sizeof a float int: %id byte(s)\n", sizeof(float));
+	printf("Sizeof a char: %ld byte(s)\n", sizeof(char));
+		printf("Sizeof an int: %ld byte(s)\n", sizeof(int));
+			printf("Sizeof a long int: %ld bytes(s)\n", sizeof(long int));
+				printf("Sizeof long long int: %ld byte(s)\n", sizeof(long long int));
+					printf("Sizeof a float int: %ld byte(s)\n", sizeof(float));
 						return (0);
 }
