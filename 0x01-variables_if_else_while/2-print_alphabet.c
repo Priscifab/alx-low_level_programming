4e51452df9f9eb6f4ca59d1348ba35ecb4ed8873
Prisicfab <106880543+Priscifab@users.noreply.github.com>
2022-10-15 16:22:44 -0700

    message

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
+ * main - Prints the alphabet in lowercase.
  *
- * Return: Always 0 (Success)
+ * Return   Always + (Success)
  */
 int main(void)
 {
-		char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-			int i;
-			for  (i = 0; i < 26; i++)
-						{
-								putchar(alp[i]);
-									}
-					putchar('\n');
-						return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
