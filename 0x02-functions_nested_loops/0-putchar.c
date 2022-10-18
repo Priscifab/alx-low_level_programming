f3635ba356fe262a063b4bdd1a73abe910e24e6f
Prisicfab <106880543+Priscifab@users.noreply.github.com>
2022-10-18 15:09:18 -0700

    hmm

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
+
 /**
  * main - print -putchar
  *
@@ -8,14 +9,13 @@
 
 int main(void);
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
 	_putchar('\n');
+
+
 	return (0);
 }
