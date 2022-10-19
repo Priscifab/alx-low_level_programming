d121e982e2d54e986d834ba5a23caeff477b4903
Prisicfab <106880543+Priscifab@users.noreply.github.com>
2022-10-19 16:29:45 -0700

    task 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two nmbers
+ * @a: the fist number
+ * @b: the second number
+ * Return: number the additonal of both
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
