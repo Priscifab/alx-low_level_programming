2b06ac1e6bc2dc820a14e7bf57f6ee8a9ec49068
Priscifab <106880543+Priscifab@users.noreply.github.com>
2022-11-04 15:32:31 -0700

    task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,23 +1,28 @@
-/*
- * File: 2-args.c
- * Auth: Gedeon Obae Gekonge
- */
-
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - Prints all arguments it receives.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always 0.
+ *main - product of 2 numbers
+ *@argc: first parameter
+ *@argv: second parameter
+ *DESCRIPTION: outputs the product if 2 numbers
+ *Return: Always (0) Success
  */
+
 int main(int argc, char *argv[])
 {
-	int arg;
-
-	for (arg = 0; arg < argc; arg++)
-		printf("%s\n", argv[arg]);
-
-	return (0);
+int a, b, c, product;
+char error[10] = "Error";
+if (argc != 3)
+{
+printf("%s\n", error);
+return (1);
+}
+for (a = 0; a < argc; a++)
+{
+b = atoi(*(argv + 1)); /**b is first number*/
+c = atoi(argv[2]); /** c is second number*/
+}
+product = b *c;
+printf("%d\n", product);
+return (0);
 }
